372bb1124764eb832fc64d0dd364a0026c0da9ff
André Felipe Ricarti <ricarti.182@gmail.com>
2018-11-09 14:08:54 -0200

    Made some improvements

--- app/src/main/java/br/com/mydancer/mydancer/services/FirebaseService.java
+++ app/src/main/java/br/com/mydancer/mydancer/services/FirebaseService.java
@@ -1,12 +1,19 @@
 package br.com.mydancer.mydancer.services;
 
 import br.com.mydancer.mydancer.model.FirebaseUserToken;
-import br.com.mydancer.mydancer.model.LoginBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface FirebaseService {
     @POST("FirebaseUserTokens")
     Call<Void> postFirebaseUserToken(@Body FirebaseUserToken firebaseUserToken);
+
+    @GET("FirebaseUserTokens")
+    Call<FirebaseUserToken> GetFirebaseUserTokens();
+
+    @POST("Firebase/CloudMessaging")
+    Call<Void> cloudMessaging(@Body FirebaseUserToken firebaseUserToken);
 }
--- app/src/main/java/br/com/mydancer/mydancer/ui/activity/CallPersonalDancerActivity.java
+++ app/src/main/java/br/com/mydancer/mydancer/ui/activity/CallPersonalDancerActivity.java
@@ -3,11 +3,20 @@ package br.com.mydancer.mydancer.ui.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.List;
 
 import br.com.mydancer.mydancer.R;
 import br.com.mydancer.mydancer.model.Event;
+import br.com.mydancer.mydancer.model.FirebaseUserToken;
+import br.com.mydancer.mydancer.retrofit.RetrofitInicializador;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CallPersonalDancerActivity extends AppCompatActivity {
 
@@ -20,5 +29,43 @@ public class CallPersonalDancerActivity extends AppCompatActivity {
 
         TextView eventTitle = findViewById(R.id.call_personal_title);
         eventTitle.setText((String) getIntent.getSerializableExtra("tituloEvento"));
+
+        Button btCall = findViewById(R.id.call_personal_call);
+        btCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<FirebaseUserToken> call = new RetrofitInicializador().getFirebaseService().GetFirebaseUserTokens();
+                call.enqueue(new Callback<FirebaseUserToken>() {
+                    @Override
+                    public void onResponse(Call<FirebaseUserToken> call, Response<FirebaseUserToken> response) {
+                        FirebaseUserToken firebaseUserToken = new FirebaseUserToken();
+                        firebaseUserToken = response.body();
+
+                        sendMessagePersonalDancer(firebaseUserToken);
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<FirebaseUserToken> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void sendMessagePersonalDancer(FirebaseUserToken firebaseUserToken) {
+        Call<Void> call = new RetrofitInicializador().getFirebaseService().cloudMessaging(firebaseUserToken);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+
+            }
+        });
     }
 }
